8f353cb19e4b3e7035170cb0eeba6f7b2dfe0fcc
Jeremy Morgan <153001285+jeremy-morgan-deque@users.noreply.github.com>
2024-08-28 15:31:58 -0400

    added another a11y issue

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
           <h1>Hello World!</h1>
           <p>This page has a11y issues.</p>
         </div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <p></p>
+        </ul>
       </header>
     </div>
   );
