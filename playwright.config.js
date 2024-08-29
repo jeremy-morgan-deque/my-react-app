2abfe645cd2beb0b9d1fb3b8605f24da6ff38268
Jeremy Morgan <153001285+jeremy-morgan-deque@users.noreply.github.com>
2024-08-29 13:34:54 -0400

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,3 +1,9 @@
 const { defineConfig } = require('@playwright/test')
 
-module.exports = defineConfig({ testDir: './tests' })
+module.exports = defineConfig({ 
+    testDir: './tests',
+    webServer: {
+        command: 'npm run start',
+        port: 3000,
+     },
+})
