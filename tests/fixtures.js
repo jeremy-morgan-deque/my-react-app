7930e47ff3e8eacb9a6c9e889dc423b2152708f8
Jeremy Morgan <153001285+jeremy-morgan-deque@users.noreply.github.com>
2024-08-29 11:31:33 -0400

    changed devhub api key env var

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -2,13 +2,13 @@ const { playwrightTest } = require('@axe-core/watcher')
 const assert = require('assert')
 
 // Get your axe Developer Hub configuration from environment variables.
-const { AXE_DEVHUB_API_KEY, AXE_DEVHUB_SERVER = 'https://axe.deque.com' } = process.env;
-assert(AXE_DEVHUB_API_KEY, 'AXE_DEVHUB_API_KEY is required')
+const { AXE_DEVHUB_API_KEY_MY_REACT_APP, AXE_DEVHUB_SERVER_MY_REACT_APP = 'https://axe.deque.com' } = process.env;
+assert(AXE_DEVHUB_API_KEY_MY_REACT_APP, 'AXE_DEVHUB_API_KEY is required')
 
 module.exports = playwrightTest({
   axe: {
-    apiKey: AXE_DEVHUB_API_KEY,
-    serverURL: AXE_DEVHUB_SERVER
+    apiKey: AXE_DEVHUB_API_KEY_MY_REACT_APP,
+    serverURL: AXE_DEVHUB_SERVER_MY_REACT_APP
   },
   headless: false,
   args: ['--headless=new']
